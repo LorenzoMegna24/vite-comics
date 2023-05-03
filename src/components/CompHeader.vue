d10ba6c3dca18084d909152c23fd243046968d0a
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-03 16:46:02 +0200

    Header completato

---
+++ src/components/CompHeader.vue
@@ -0,0 +1,34 @@
+<script>
+   export default{
+        name: "CompHeader",
+   }
+</script>
+
+<template>
+  <header>
+    <nav>
+      <div class="logo">
+        <a href="/"><img src="../../public/img/dc-logo.png" alt=""></a>
+      </div>
+      <div>
+        <ul>
+          <li><a href="/">characters</a></li>
+          <li><a href="/">comics</a></li>
+          <li><a href="/">movies</a></li>
+          <li><a href="/">tv</a></li>
+          <li><a href="/">games</a></li>
+          <li><a href="/">collectibles</a></li>
+          <li><a href="/">videos</a></li>
+          <li><a href="/">fans</a></li>
+          <li><a href="/">news</a></li>
+          <li><a href="/">shop</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
