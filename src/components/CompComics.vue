67eb2a4a4f0a46dc474d8eac1b49609263fe2f5d
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-04 15:30:36 +0200

    Creazione componente figlio

---
+++ src/components/CompComics.vue
@@ -0,0 +1,20 @@
+<script>
+  export default{
+    name: "CompComics",
+    props: ['DettaglioComic']
+   }
+</script>
+
+<template>
+  <div class="card">
+    <img :src="DettaglioComic.thumb" alt="">
+    <h3>{{ DettaglioComic.series }}</h3>
+    <span>{{ DettaglioComic.type }}</span>
+    <span>{{ DettaglioComic.price }}</span>
+  </div>
+
+</template>
+
+<style lang="scss" scoped >
+
+</style>
